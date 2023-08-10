77bf9260495407f8a6f3521595b57ae4d606b48c
AR Assignments <49037725+ARassignments@users.noreply.github.com>
2023-08-09 23:38:45 -0700

    dashboard

--- app/src/main/java/com/example/soccerxplorer/DashboardActivity.java
+++ app/src/main/java/com/example/soccerxplorer/DashboardActivity.java
@@ -3,6 +3,7 @@ package com.example.soccerxplorer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
